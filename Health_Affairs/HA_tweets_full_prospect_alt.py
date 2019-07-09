8dfaa2473d0fbbe8d35578ebf17f3bb063f8572d
unknown <lsp52@cornell.edu>
2019-07-09 08:51:28 -0400

    u

--- Health_Affairs/HA_tweets_full_prospect_alt.py
+++ Health_Affairs/HA_tweets_full_prospect_alt.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 df = pd.read_excel('output/HA_articles_parsed_manual_v2.xlsx')
 df = df.rename({'Unnamed: 0':'id'}, axis=1)
-df['Title'] = df.Title.str.replace('-', ' ').str.replace('/', ' ').str.replace('\', ' ').str.replace(':', ' ')
+df['Title'] = df.Title.str.replace('-', ' ').str.replace('/', ' ').str.replace(':', ' ')
 df['Title_alt'] = df['Title'] + ' (OR Health Affairs OR healthaffairs OR health_affairs)'
 if df['Publication Date'].dtype == 'object':
     try:
@@ -33,12 +33,12 @@ count_df = pd.DataFrame(columns=['Title','Tweet Counts'])
 for i in article_df.iterrows():
     print('scraping article #' + str(i[0]))
     tweet_df = extract_tweets(i[1]['Title'],i[1]['Keywords'],i[1]['Start_Date'],
-        i[1]['End_Date'], get_replies = False, get_retweets = False, halt_count=100)
+        i[1]['End_Date'], get_replies = False, get_retweets = True)
     count_df = count_df.append(pd.Series([i[1]['Title'],len(tweet_df.index)
         + tweet_df.retweet_count.sum()],index = ['Title','Tweet Counts']),
         ignore_index = True, sort = False)
     if len(tweet_df.index) !=0 :
         tweet_df.sort_values('created_at',ascending=False).to_csv('output/prospect_alt/'+
-            str(i[0]) + '_' +  i[1]['Title'][0:40] '_tweet_data.csv', index = False, encoding='utf-8')
+            str(i[0]) + '_' +  i[1]['Title'][0:40]+ '_tweet_data.csv', index = False, encoding='utf-8')
 
 count_df.to_csv("output/prospect_alt/tweet_counts.csv")
